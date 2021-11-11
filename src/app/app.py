878fe6b81709829dbc8745f0dc3a0265a11311ff
Taichi306 <saitaichi0423@gmail.com>
2021-11-11 18:19:43 +0900

    'a'

--- src/app/app.py
+++ src/app/app.py
@@ -1,29 +1,60 @@
 from flask import Flask, render_template, request, redirect, session, url_for
 from datetime import timedelta
+from flask_socketio import SocketIO, join_room, leave_room, emit
 import mysql.connector
 
 app = Flask(__name__)
 
 # セッション設定
-app.secret_key = 'aaa'
-app.config['SECRET_KEY'] = 'aaa'
-app.config['SESSION_TYPE'] = 'filesystem'
+app.secret_key = 'secret!'
+app.config['SECRET_KEY'] = 'secret!'
+app.config['SESSION_TYPE'] = 'secret!'
+
+# SocketIO設定
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
-    if request.method == 'GET':
+    if request.method == 'POST':
+        room = request.form['room']
+        session['room'] = room
+        # # ----------------------------------------------------------------------
+        # session.permanent = True
+        # session.modified = True
+        # app.permanent_session_lifetime = timedelta(minutes=5)
+        # # ----------------------------------------------------------------------
+        return render_template('socket.html', session=session)
+    else:
         return render_template('main.html')
 
-    if request.method == 'POST' and 'room_name' in request.form:
-        room = request.form['room_name']
-        session.permanent = True
-        session.modified = True
-        app.permanent_session_lifetime = timedelta(minutes=5)
-        session['room'] = room
-        return render_template('source.html')
+
+@app.route('/socket', methods=['GET', 'POST'])
+def test():
+    if (session['room'] is not None):
+        return render_template('socket.html', session=session)
+    else:
+        return redirect(url_for('/'))
+
+
+@socketio.on('join', namespace='/socket')
+def join(message):
+    room = session['room']
+    join_room(room)
+    emit(room=room)
+
+
+@socketio.on('text', namespace='/socket')
+def chat(message):
+    room = session['room']
+    emit('message', {'msg': message['msg']}, room=room)
 
 
 @app.route("/game")
 def game():
-    room = session['room']
-    return render_template('source.html', room=room)
+    return render_template('source.html')
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
---
+++ src/app/static/socketio.js
@@ -0,0 +1,39 @@
+var socket;
+$(document).ready(function(){
+    socket = io.connect('http://' + document.domain + ':' + location.port + '/socket');
+
+    var chat = document.getElementById('chat');
+    var text = document.getElementById('text');
+    var form = document.getElementById('form');
+
+    socket.on('connect', function(){
+        socket.emit('join', {});
+    });
+
+    // socket.on('status', function(data){
+    //     var item = document.createElement('li');
+    //
+    //     for (let i = 0; i < data['msg'].length; i++){
+    //         item = document.createElement('li');
+    //         item.textContent = data['msg'];
+    //         chat.appendChild(item);
+    //     }
+    //     window.scrollTo(0, document.body.scrollHeight);
+    // });
+
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+        if (text.value){
+            socket.emit('text', {msg: text.value});
+            text.value = '';
+        }
+    });
+
+    socket.on('message', function(data){
+        var item = document.createElement('li');
+        item.textContent = data['msg'];
+        chat.appendChild(item);
+        window.scrollTo(0, document.body.scrollHeight);
+    });
+
+});
